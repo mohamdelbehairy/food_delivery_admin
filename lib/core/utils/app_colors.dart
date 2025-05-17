64657ff8a007e63a73862e362f9c86ff85c36abc
mohamdelbehairy <rahmawemohamed@gmail.com>
2025-05-17 14:15:27 +0300

    create styles,colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const primaryColor = Color(0xff4280EF);
+  static const blackColor = Color(0xff2C2C2C);
+}
---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+abstract class Styles {
+  // bold
+  static const bold42 = TextStyle(
+      fontSize: 42,
+      fontWeight: FontWeight.bold,
+      fontFamily: "PlusJakartaSans",
+      color: AppColors.primaryColor);
+
+  // semi bold
+  static const semiBold16 = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      fontFamily: "PlusJakartaSans",
+      color: AppColors.blackColor);
+
+  static const semiBold14 = TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+      fontFamily: "PlusJakartaSans",
+      color: AppColors.blackColor);
+}
